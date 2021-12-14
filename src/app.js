9c3520b3bf5a85cfcc42281552f2626bad6030ac
Zink Wu <wugu2599l@gmail.com>
2021-12-15 06:01:13 +0800

    basic unit test

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Button from './button.vue'
 import Icon from './icon'
 import ButtonGroup from './button-group'
+import { expect } from 'chai'
 
 Vue.component('r-button', Button)
 Vue.component('r-icon', Icon)
@@ -9,9 +10,94 @@ Vue.component('r-button-group', ButtonGroup)
 
 new Vue({
   el: "#app",
-  data(){
+  data() {
     return {
       loading: false
     }
   }
 })
+
+
+// unit test
+{
+  const Constructor = Vue.extend(Button)
+  const vm = new Constructor({
+    propsData: {
+      icon: 'settings'
+    }
+  })
+  vm.$mount()
+  let useEl = vm.$el.querySelector('use')
+  let href = useEl.getAttribute('xlink:href')
+  expect(href).to.eq('#i-settings')
+  vm.$el.remove()
+  vm.$destroy()
+}
+
+{
+  const Constructor = Vue.extend(Button)
+  const vm = new Constructor({
+    propsData: {
+      icon: 'settings',
+      loading: true
+    }
+  })
+  vm.$mount()
+  let useEl = vm.$el.querySelector('use')
+  let href = useEl.getAttribute('xlink:href')
+  expect(href).to.eq('#i-loading')
+  vm.$el.remove()
+  vm.$destroy()
+}
+
+{
+  const div = document.createElement('div')
+  document.body.appendChild(div)
+  const Constructor = Vue.extend(Button)
+  const vm = new Constructor({
+    propsData: {
+      icon: 'settings'
+    }
+  })
+  vm.$mount(div)
+  let svg = vm.$el.querySelector('svg')
+  let order = window.getComputedStyle(svg).order
+  expect(order).to.eq('1')
+  vm.$el.remove()
+  vm.$destroy()
+}
+
+{
+  const div = document.createElement('div')
+  document.body.appendChild(div)
+  const Constructor = Vue.extend(Button)
+  const vm = new Constructor({
+    propsData: {
+      icon: 'settings',
+      iconPosition: 'right'
+    }
+  })
+  vm.$mount(div)
+  let svg = vm.$el.querySelector('svg')
+  let order = window.getComputedStyle(svg).order
+  expect(order).to.eq('2')
+  vm.$el.remove()
+  vm.$destroy()
+}
+
+{
+  const Constructor = Vue.extend(Button)
+  const vm = new Constructor({
+    propsData: {
+      icon: 'settings',
+      iconPosition: 'right'
+    }
+  })
+  vm.$mount()
+  vm.$on('click', function(){
+    expect(1).to.eq(1)
+  })
+  vm.$el.click()
+  vm.$el.remove()
+  vm.$destroy()
+}
