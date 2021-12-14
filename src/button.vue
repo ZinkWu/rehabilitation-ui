c3eb7263193105653f03985368ffe14451ad39c9
Zink Wu <wugu2599l@gmail.com>
2021-12-14 13:23:06 +0800

    add icon

--- src/button.vue
+++ src/button.vue
@@ -1,29 +1,58 @@
 <template>
-  <button class="r-button">按钮</button>
+  <button class="r-button" :class="{ [`icon-${iconPosition}`]: true }">
+    <svg class="icon" aria-hidden="true" v-if="icon">
+      <use :xlink:href="`#i-${icon}`"></use>
+    </svg>
+    <div class="content">
+      <slot></slot>
+    </div>
+  </button>
 </template>
 
 <script>
-export default {};
+export default {
+  props: {
+    icon: {
+      type: String,
+      requierd: false,
+    },
+    iconPosition: {
+      type: String,
+      requierd: false,
+      default: "left",
+      validator: function(value){
+        return ['left', 'right'].indexOf(value) !== -1
+      }
+    },
+  },
+  data() {
+    return {};
+  },
+};
 </script>
 
 <style lang="scss">
 .r-button {
+  color: #000;
   font-size: var(--font-size);
   height: var(--button-height);
   padding: 0 1em;
-  font: inherit;
   border-radius: var(--border-radius);
   border: 1px solid var(--border-color);
   background: var(--button-bg);
+  display: inline-flex;
+  justify-content: center;
+  align-items: center;
+  vertical-align: middle;
+  > .icon {order: 1; margin-right: .1em}
+  > .content {order: 2;}
+  &.icon-right {
+    > .icon {order: 2; margin-left: .1em; margin-right: 0;}
 
-  &:hover {
-    border-color: var(--border-color-hover);
+    > .content {order: 1;}
   }
-  &:active {
-    background: var(--button-active-bg);
-  }
-  &:focus {
-    outline: none;
-  }
+  &:hover {border-color: var(--border-color-hover);}
+  &:active {background: var(--button-active-bg);}
+  &:focus {outline: none;}
 }
 </style>
