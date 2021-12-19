56dc3dfafc4261f5b41bcf4477122bb53fb4da7e
Zink Wu <wugu2599l@gmail.com>
2021-12-20 00:31:54 +0800

    basic grid

--- src/app.js
+++ src/app.js
@@ -3,11 +3,15 @@ import Button from './button'
 import Icon from './icon'
 import ButtonGroup from './button-group'
 import Input from './input'
+import Row from './row'
+import Col from './col'
 
 Vue.component('r-button', Button)
 Vue.component('r-icon', Icon)
 Vue.component('r-button-group', ButtonGroup)
 Vue.component('r-input', Input)
+Vue.component('r-row', Row)
+Vue.component('r-col', Col)
 
 new Vue({
   el: "#app",
---
+++ src/col.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="col" :class="[`col-${span}`]">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    span: {
+      type: [Number, String],
+      required: false,
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.col {
+  height: 100px;
+  background: gray;
+  border: 1px solid red;
+  width: 50%;
+
+  $class-prefix: col-;
+  @for $n from 1 through 24 {
+    &.#{$class-prefix}#{$n} {
+      width: calc($n/24) * 100%;
+    }
+  }
+}
+</style>
