c7bf3574c035d37a3fc8ecddec83342956ccae84
Zink Wu <wugu2599l@gmail.com>
2021-12-24 05:08:48 +0800

    basic layout components

--- src/app.js
+++ src/app.js
@@ -5,6 +5,11 @@ import ButtonGroup from './button-group'
 import Input from './input'
 import Row from './row'
 import Col from './col'
+import Layout from './layout'
+import Header from './header'
+import Sider from './sider'
+import Content from './content'
+import Footer from './footer'
 
 Vue.component('r-button', Button)
 Vue.component('r-icon', Icon)
@@ -12,6 +17,11 @@ Vue.component('r-button-group', ButtonGroup)
 Vue.component('r-input', Input)
 Vue.component('r-row', Row)
 Vue.component('r-col', Col)
+Vue.component('r-layout', Layout)
+Vue.component('r-header', Header)
+Vue.component('r-sider', Sider)
+Vue.component('r-content', Content)
+Vue.component('r-footer', Footer)
 
 new Vue({
   el: "#app",
---
+++ src/sider.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="sider">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "RehabSider"
+}
+</script>
+
+<style lang="scss" scoped>
+.sider{}
+</style>
